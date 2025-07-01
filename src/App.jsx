807173e6bfe6a645183e104f8970831cca294cdb
Ramalakshmi <iramalakshmiramya@gmail.com>
2025-07-01 18:10:09 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import FetchData from './Components/FetchData'
-
-function App() {
-  return (
- <>
- <FetchData/>
- </>
-  )
-}
-
-export default App
+ import FetchData from './Components/FetchData'
+ function App() {
+   return (
+  <>
+  <FetchData/>
+  </>
+   )
+ }
+ export default App
